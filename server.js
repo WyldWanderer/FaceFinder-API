53afb142d9b7ce1e661bc55f2d80be17a5e74a4e
Tyler <living2snowboard@gmail.com>
2020-07-10 14:27:19 -0600

    July 10 commit

--- server.js
+++ server.js
@@ -2,51 +2,32 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const bcrypt = require('bcrypt-nodejs');
 const cors = require('cors');
+const knex =require('knex') 
+
+const db = knex({
+    client: 'pg',
+    connection: {
+      host : '127.0.0.1',
+      user : 'tylerscott',
+      password : '',
+      database : 'smartbrain'
+    }
+  });
+
+db.select('*').from('users').then(data => {
+    console.log(data);
+});
 
 const app = express();
 
 app.use(bodyParser.json())
 app.use(cors())
 
-const database = {
-    users: [
-        {
-            id: '123',
-            name: 'John',
-            email: 'john@gmail.com',
-            password: 'cookies',
-            entries: 0,
-            joined: new Date()
-        },
-        {
-            id: '124',
-            name: 'Sally',
-            email: 'sally@gmail.com',
-            password: 'bananas',
-            entries: 0,
-            joined: new Date()
-        }
-    ],
-    /* login: [
-        {
-            id: '987',
-            hash:'',
-            email: 'john@gmail.com'
-        }
-    ] */
-}
-
 app.get('/', (req, res) => {
     res.send(database.users);
 })
 
 app.post('/signin', (req, res) => {
-   /* bcrypt.compare("bacon", hash, function(err, res) {
-        // res == true
-    });
-    bcrypt.compare("veggies", hash, function(err, res) {
-        // res = false
-    }); */
     if (req.body.email === database.users[0].email && 
         req.body.password === database.users[0].password) {
             res.json('success')
@@ -58,16 +39,11 @@ app.post('/signin', (req, res) => {
 
 app.post('/register', (req, res) => {
     const { email, name, password } = req.body;
-    bcrypt.hash(password, null, null, function(err, hash) {
-        console.log(hash)
-    });
-    database.users.push({ 
-        id: '125',
-        name: name,
-        email: email,
-        entries: 0,
-        joined: new Date()
-    })
+    db('users').insert({
+        email : email, 
+        name : name, 
+        joined : new Date()
+    }).then(console.log)
     res.json(database.users[database.users.length-1]);
 })
 
