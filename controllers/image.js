ae8f479e387971cfcb9e69a4e826b8846837ed79
Tyler <living2snowboard@gmail.com>
2020-07-22 21:56:10 -0600

    Security updates to not show API key during post

--- controllers/image.js
+++ controllers/image.js
@@ -1,3 +1,19 @@
+const Clarifai = require('clarifai');
+const { json } = require('body-parser');
+
+const app = new Clarifai.App({
+    apiKey: "b3b9e57a6b734f5897847c4888965ff1"
+  });;
+
+const handleApiCall = (req, res) => {
+    app.models
+        .predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
+        .then(data => { 
+            res.json(data);
+        })
+        .catch(err => res.status(400).json('unable to access API'))
+}
+
 const handleImage = (req, res, db) => {
     const { id } = req.body;
     db('users').where('id', '=', id)
@@ -10,5 +26,6 @@ const handleImage = (req, res, db) => {
 }
 
 module.exports = { 
-    handleImage : handleImage
+    handleImage : handleImage,
+    handleApiCall : handleApiCall
 };
