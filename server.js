e7fb6875f33fbcc9c2ef1576a21593f579c932c6
Tyler <living2snowboard@gmail.com>
2020-07-25 14:42:40 -0600

    adding database connection

--- server.js
+++ server.js
@@ -12,10 +12,8 @@ const image = require('./controllers/image');
 const db = knex({
     client: 'pg',
     connection: {
-      host : 'postgresql-regular-98204',
-      user : 'tylerscott',
-      password : '',
-      database : 'smartbrain'
+      host : process.env.DATABASE_URL,
+      ssl : true
     }
   });
 
